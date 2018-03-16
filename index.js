2f4ce64a804ca5e07786994091c81b54b092bb8c
Alexis Jamet <alexis.jamet@epitech.eu>
2018-03-16 15:06:25 +0100

    tentative de connection

--- index.js
+++ index.js
@@ -1,66 +1,56 @@
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47, lng: 5},
-    zoom: 5,
-    mapTypeId: 'roadmap'
-  });
+// $(function SubmitForm(){
+//
+//   var $orders = $('#orders');
+//   var $firstname = $('#firstname');
+//   var $lastname = $('#lastname');
+//   var $login = $('#login');
+//   var $phone = $('#phone');
+//   var $birthday = $('#birthday');
+//
+//
+// $.ajax({
+//   type: "GET",
+//   url: 'http://163.5.84.234/api/user/all',
+//
+//   success: function() {
+//     // $.each(orders, function(i, order) {
+//     //   $orders.apprend('<li>name' + order.firstname + ', drink' + order.lastname + '</li>');
+//     // };
+//     alert("ça marche");
+//   },
+//   error: function() {
+//     alert('LOOOOL');
+//   }
+// });
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // $.ajax({
+  //   type: "POST",
+  //   url: 'http://163.5.84.234/api/user/all',
+  //   data, orders,
+  //
+  //   success: function(neworders) {
+  //     $orders.apprend('<li>name' + neworder.name + ', drink' + neworder.drink + '</li>');
+  //   },
+  //   error: function() {
+  //     alert('LOOOOL');
+  //   }
+  // });
+// })
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+function ajaxGet() {
+   var req = new XMLHttpRequest();
+   req.open("GET", "http://163.5.84.234/api/user/all");
+   req.setRequestHeader('Access-Control-Allow-Origin', '*');
+   req.setRequestHeader('Access-Control-Allow-Headers', '*');
+   req.setRequestHeader('Content-Type', 'application/json');
+   req.addEventListener("load", function () {
+       if (req.status >= 200 && req.status < 400) {
+         console.log("IN")
+       } else {
+           console.error(req.status + " " + req.statusText + " " + url);
+       }
+   });
+   req.addEventListener("error", function () {
+   });
+   req.send(null);
 }
